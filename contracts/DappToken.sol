479de0fc42bdb0c0f1a8271ebfbf01bec3ad1630
unknown <parthsujalshah@gmail.com.com>
2020-01-17 00:03:42 +0530

    2. Transfer Token

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -1,12 +1,36 @@
 pragma solidity ^0.5.0;
 
 contract DappToken {
-    //Constructor
-    //Set tokens
-    //Read total number of tokens
     uint256 public totalSupply;
+    string public name = 'DApp Token';
+    string public symbol = 'DAPP';
+    string public standard = 'DApp Token v1.0'; // Not part of erc20
 
-    constructor () public {
-        totalSupply = 1000000;
+    event Transfer(
+        address indexed _from,
+        address indexed _to,
+        uint256 _value
+    );
+
+    mapping(address => uint256) public balanceOf;
+
+    constructor (uint256 _initialSupply) public {
+        balanceOf[msg.sender] = _initialSupply;
+        totalSupply = _initialSupply;
+    }
+
+    // Transfer
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        // Exception if account doesn't have enough
+        require(balanceOf[msg.sender] >= _value);
+        
+        // Transfer the balance
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+
+        emit Transfer(msg.sender, _to, _value);
+
+        // return a boolean
+        return true;
     }
 }
